7e554d2e7fe602d0aa3fe9e9a5bf2b7054fb2563
tyui592 <tyui592@gmail.com>
2021-06-15 15:42:18 +0900

    update ResNet to adjust "num_class"

--- network.py
+++ network.py
@@ -136,16 +136,16 @@ class ResNet(nn.Module):
         return out
 
 def ResNet18(num_class=10):
-    return ResNet(BasicBlock, [2,2,2,2])
+    return ResNet(BasicBlock, [2,2,2,2], num_class)
 
 def ResNet34(num_class=10):
-    return ResNet(BasicBlock, [3,4,6,3])
+    return ResNet(BasicBlock, [3,4,6,3], num_class)
 
 def ResNet50(num_class=10):
-    return ResNet(Bottleneck, [3,4,6,3])
+    return ResNet(Bottleneck, [3,4,6,3], num_class)
 
 def ResNet101(num_class=10):
-    return ResNet(Bottleneck, [3,4,23,3])
+    return ResNet(Bottleneck, [3,4,23,3], num_class)
 
 def ResNet152(num_class=10):
-    return ResNet(Bottleneck, [3,8,36,3])
+    return ResNet(Bottleneck, [3,8,36,3], num_class)
